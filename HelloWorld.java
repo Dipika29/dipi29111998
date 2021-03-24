a533efde3f16b9a350d7fa1fb2b9da249a457db1
Dipika29 <81138406+Dipika29@users.noreply.github.com>
2021-03-24 19:18:59 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
 class HelloWorld{
   public static void main(String []args){
     System.out.println("Hello World");
+    add(10,20);
   }
-  public static void add(int num1.int num2){
+  public static void add(int num1,int num2){
     System.out.println(num1+num2);
   }
 }
